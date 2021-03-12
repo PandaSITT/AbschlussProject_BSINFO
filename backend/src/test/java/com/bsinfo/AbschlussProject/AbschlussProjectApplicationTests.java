765e671824dd60c5ea4764fe9a357b4597f060c8
Luca Marino <lmarino@united-internet.de>
2021-03-12 09:02:31 +0100

    added first version of spring backend

---
+++ backend/src/test/java/com/bsinfo/AbschlussProject/AbschlussProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsinfo.AbschlussProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbschlussProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
